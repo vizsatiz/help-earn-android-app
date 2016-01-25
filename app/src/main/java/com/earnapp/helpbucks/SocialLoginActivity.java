b632cbf6115ef5ac3b1473e501ccfa590ea2b7c7
vishnu.satis <vishnu.satis@kony.com>
2016-01-25 19:37:27 +0530

    Added changes for creating tab views with three tab options

--- app/src/main/java/com/earnapp/helpbucks/SocialLoginActivity.java
+++ app/src/main/java/com/earnapp/helpbucks/SocialLoginActivity.java
@@ -1,26 +1,86 @@
 package com.earnapp.helpbucks;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
+import com.facebook.AccessToken;
+import com.facebook.AccessTokenTracker;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
 import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 
 public class SocialLoginActivity extends AppCompatActivity {
+
+    CallbackManager callbackManager;
+    AccessTokenTracker accessTokenTracker;
+    final Context context = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getSupportActionBar().hide();
+        //getSupportActionBar().hide();
         // Initializing FaceBook SDK
         FacebookSdk.sdkInitialize(getApplicationContext());
+        callbackManager = CallbackManager.Factory.create();
+        accessTokenTracker = new AccessTokenTracker() {
+            @Override
+            protected void onCurrentAccessTokenChanged(
+                    AccessToken oldAccessToken,
+                    AccessToken currentAccessToken) {
+                updateWithToken(currentAccessToken);
+
+            }
+        };
+        LoginManager.getInstance().registerCallback(callbackManager,
+            new FacebookCallback<LoginResult>() {
+                @Override
+                public void onSuccess(LoginResult loginResult) {
+                    Intent intent = new Intent(context, TaskListActivity.class);
+                    startActivity(intent);
+                }
+
+                @Override
+                public void onCancel() {
+                    // App code
+                }
+
+                @Override
+                public void onError(FacebookException exception) {
+                    // App code
+                }
+            });
         setContentView(R.layout.activity_social_login);
+    }
+
+    /**
+     * This function checks wether the current token is l;
+     *
+     * @param token
+     */
+    private void updateWithToken(AccessToken token) {
+        if(token!=null){
+            Intent intent = new Intent(context, TaskListActivity.class);
+            startActivity(intent);
+        }
+        else {
+            //showFragment(login);
+        }
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-
         // Logs 'install' and 'app activate' App Events.
         AppEventsLogger.activateApp(this);
     }
@@ -28,8 +88,13 @@ public class SocialLoginActivity extends AppCompatActivity {
     @Override
     protected void onPause() {
         super.onPause();
-
         // Logs 'app deactivate' App Event.
         AppEventsLogger.deactivateApp(this);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
     }
 }
